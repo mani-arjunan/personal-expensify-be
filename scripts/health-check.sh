9d9590526f84737ce58e47b85e529a003fa8fcd4
Manikandan <manikandan@pop-os.localdomain>
2023-11-28 16:02:30 +0530

    add docker configuration and setup postgres as a docker service

---
+++ scripts/health-check.sh
@@ -0,0 +1,26 @@
+MAX_RETRIES=60
+CURRENT_RETRY=0
+DELAY=1
+
+throw_error() {
+  MESSAGE=$1
+  RED="\e[31m"
+
+  printf "\n${RED}ERR!: ${MESSAGE}\n\n" 1>&2
+  exit 1;
+}
+
+if [ -z $1 ]; then
+ throw_error "Missing required parameter. \n\n health-check.sh [service_name]" 
+fi;
+
+
+while [ ${CURRENT_RETRY} -lt ${MAX_RETRIES}]
+do
+  CURRENT_RETRY=${CURRENT_RETRY}+1
+  echo "[Attempt ${CURRENT_RETRY}/${MAX_RETRIES}]: waiting for $1 to be healthy"
+
+  COMMAND="$(docker-compose ps $1)"
+  echo "${COMMAND}"
+
+  if grep -q '(healthy)' <<< "$COMMAND";
